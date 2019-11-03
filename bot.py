7c620a6f336643bc991d0dff60ae33f9f351d10e
W1z3 <pasha18021@gmail.com>
2019-11-03 17:18:51 +0300

    Added CommandHandlers, /start command response

--- bot.py
+++ bot.py
@@ -1,10 +1,19 @@
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler
 
 
 
 def main():
     bot = Updater('1027387265:AAGoKkRBl04IFFneRDRKcsh7H7rZIAGLOns')
+
+    dp = bot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+
     bot.start_polling()
     bot.idle()
+
+def greet_user(bot, update):
+    reply_text = "Добро пожаловать! Чем я могу быть полезен?"
+    print(reply_text)
+    update.message.reply_text(reply_text) 
 
 main()

3485a0208664058f5baace341963a6d0812a2d4c
W1z3 <pasha18021@gmail.com>
2019-11-04 02:25:01 +0300

    Improved logging system, few new functions

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
 
@@ -15,14 +15,29 @@ def main():
 
     dp = bot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("info", user_info))
+    dp.add_handler(MessageHandler(Filters.text, user_dialogue))
 
     bot.start_polling()
     logging.info("Bot started successfully.")
     bot.idle()
 
 def greet_user(bot, update):
-    reply_text = "Привет! Чем я могу быть полезен?"
+    reply_text = "S-sup!"
     update.message.reply_text(reply_text)
-    logging.info(reply_text)
+    logging.info("(Chat ID: %s) %s issued command: /start", update.message.chat.id, update.message.chat.username)
+    logging.info("(Chat ID: %s) [Bot] %s", update.message.chat.id, reply_text)
+
+def user_info(bot, update):
+    user_info = "{}'s account info:\nID: {}\nUsername: {}\nFirst name: {}".format(update.message.chat.first_name, update.message.chat.id, update.message.chat.username, update.message.chat.first_name)
+    update.message.reply_text(user_info)
+    logging.info("(Chat ID: %s) %s issued command: /info", update.message.chat.id, update.message.chat.username)
+    logging.info("(Chat ID: %s) [Bot] %s", update.message.chat.id, user_info)
+
+def user_dialogue(bot, update):
+    user_text = update.message.text
+    update.message.reply_text(user_text)
+    logging.info("(Chat ID: %s) <%s> %s", update.message.chat.id, update.message.chat.username, user_text)
+    logging.info("(Chat ID: %s) [Bot] %s", update.message.chat.id, user_text)
 
 main()
